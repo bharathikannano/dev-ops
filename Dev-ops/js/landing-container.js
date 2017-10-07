3106fd2748499dc24c5252fc58a41c89bdb93539
Bharathikannan <bharathikannan@live.com>
2017-10-07 10:34:38 +0530

    datepicker changes.....!

--- Dev-ops/js/landing-container.js
+++ Dev-ops/js/landing-container.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     //  $('#datetimepicker1').datetimepicker();
-    $(".container").load("/templates/studentform.html");
+    $(".container").load("/templates/studentform.html", function(data) {
+
+        $('#datetimepicker1, #datetimepicker2').datetimepicker({
+            format: 'YYYY-MM-DD',
+        }).data('autoclose', true);
+    });
+    // $(".container").load("/templates/position-form.html", function(data) {
+
+    //     $('#datetimepicker1').datetimepicker({
+    //         format: 'YYYY-MM-DD',
+    //     }).data('autoclose', true);
+    // });
+
+
+
 
 });
