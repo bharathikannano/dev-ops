c6685a0e72dee56dd302b574c8c4968060feb7cf
Bharathikannan <bharathikannan@live.com>
2017-10-11 00:28:43 +0530

    json implementation....

--- Dev-ops/js/landing-container.js
+++ Dev-ops/js/landing-container.js
@@ -1,31 +1,26 @@
 $(document).ready(function() {
-    //  $('#datetimepicker1').datetimepicker();
-    // $(".container").load("/templates/new-events.html", function(data) {
-    //
-    //     $('#datetimepicker1, #datetimepicker2').datetimepicker({
-    //         format: 'YYYY-MM-DD',
-    //     }).data('autoclose', true);
-    // });event/edit-events.html
-// $(".container").load("/templates/student/new-studentForm.html", function(data) {
-//
-//     $('#datetimepicker1, #datetimepicker2').datetimepicker({
-//         format: 'YYYY-MM-DD',
-//     }).data('autoclose', true);
-// });
-
-$('#homeSubmenu li a, #pageSubmenu li a').click(function() {
-    //alert($(this).data('val'));
-
-$(".container").load($(this).data('val') + ".html", function(data) {
-$('#sidebar').toggleClass('active');
-    $('#datetimepicker1, #datetimepicker2').datetimepicker({
-        format: 'YYYY-MM-DD',
-    }).data('autoclose', true);
-});
 
 
+    $('#homeSubmenu li a, #pageSubmenu li a').click(function() {
 
-});
+        $(".container").load($(this).data('val') + ".html", function(data) {
+            $('#sidebar').toggleClass('active');
+            $('#datetimepicker1, #datetimepicker2').datetimepicker({
+                format: 'YYYY-MM-DD',
+            }).data('autoclose', true);
+        });
+        $.ajax({
+            crossOrigin: true,
+            type: "GET",
+            url: "../data/formData.json",
+            success: function(data) {
+                $.each(data, function(i, item) {
+                    $('#' + i).val(item);
+                });
+            }
+        });
+
+    });
 
 
 });
